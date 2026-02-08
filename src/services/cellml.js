f324ce1ad08327eb3337f4bb51daa03a04a884d9
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-02-08 23:39:47 +1300

    Remove debug console.log statements
    Removed console.log statements to clean up code.

--- src/services/cellml.js
+++ src/services/cellml.js
@@ -8,7 +8,6 @@ const blobToBase64 = (blob) => {
       // reader.result is like "data:application/zip;base64,UEsDB..."
       // We only want the part after the comma
       const splitResult = reader.result.split(',')
-      console.log('Split result:', splitResult[0])
       const base64data = reader.result.split(',')[1]
       resolve(base64data)
     }
@@ -22,20 +21,15 @@ export async function createCellMLDataFragment(cellmlBlob, fileName) {
   const internalName = fileName ? (fileName.endsWith('.cellml') ? fileName : `${fileName}.cellml`) : 'model.cellml'
   zip.file(internalName, cellmlBlob)
 
-  console.log('Generating CellML zip blob...')
   const sizeInKB1 = (cellmlBlob.size / 1024).toFixed(2)
-  console.log(`CellML size: ${sizeInKB1} KB`)
 
   const zipBlob = await zip.generateAsync({
     type: 'blob',
     compression: 'DEFLATE',
     compressionOptions: { level: 9 },
   })
-  console.log('Generated zip blob:')
   const sizeInKB = (zipBlob.size / 1024).toFixed(2)
-  console.log(`Zip size: ${sizeInKB} KB`)
   const base64String = await blobToBase64(zipBlob)
-  console.log('Base64 string length:', base64String.length)
 
   const dataUri = `data:application/x.vnd.zip-cellml+zip;base64,${base64String}`
   return dataUri
