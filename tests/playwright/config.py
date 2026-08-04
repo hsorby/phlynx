fd95b67ce45b33d71155d4362477b7e7c4c4734c
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-08-04 13:56:40 +1200

    Set default testing environment to http://localhost:5173/.

--- tests/playwright/config.py
+++ tests/playwright/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 load_dotenv(".env.local")
 
-BASE_URL = os.environ.get("PHLYNX_BASE_URL", "https://phlynx.org/")
+BASE_URL = os.environ.get("PHLYNX_BASE_URL", "https://phlynx.com/")
 RESOURCE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "resources"))
 HEADLESS_MODE = os.environ.get("HEADLESS_MODE", "true").lower() == "true"
 
