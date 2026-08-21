677c035cd81205789ddfe0ae586ab2da0e1fba6f
jmdowrick <146781982+jmdowrick@users.noreply.github.com>
2026-08-22 11:35:58 +1200

    Remove unnecessary api page

--- src/router/index.js
+++ src/router/index.js
@@ -11,10 +11,6 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/api/',
-      component: WorkspaceArea,
-    },
-    {
       path: '/',
       name: 'workspace',
       component: WorkspaceArea,
